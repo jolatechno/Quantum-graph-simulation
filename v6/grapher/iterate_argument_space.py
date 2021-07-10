0836a22088b674ca77987d97302f467710263d2d
jolatechno <joseph.touzet@gmail.com>
2021-07-10 23:39:11 +0200

    corrected some errors in the grapher

--- v6/grapher/iterate_argument_space.py
+++ v6/grapher/iterate_argument_space.py
@@ -5,6 +5,23 @@ import os
 import numpy as np
 import json
 
+def print_to_json(indent, data):
+	indent_string = '\t'*indent
+
+	print(f"{ indent_string }{{")
+	for j, key in enumerate(data):
+		separator = ',' if j != len(data) - 1 else ''
+
+		stringified = data[key]
+
+		if isinstance(stringified, str):
+			stringified = "\"" + stringified + "\""
+		elif isinstance(stringified, bool):
+			stringified = "true" if stringified else "false"
+
+		print(f"\t{ indent_string }\"{ key }\" : { stringified }{ separator }")
+	print(f"\t\t}}{ ',' if i != len(rules) - 1 else '' }")
+
 parser = argparse.ArgumentParser(description='run quantum iteration for all possible arguments')
 parser.add_argument('-n', '--n-iter', type=int, default=10, help='number of iteration for each simulation')
 
@@ -31,40 +48,40 @@ n_avg = (args.end_seed - args.start_seed) * args.n_serializing
 phis = list(np.linspace(args.p0, args.p1, args.n_phi))
 tetas = list(np.linspace(args.t0, args.t1, args.n_teta))
 
-rules = None
+def make_cmd(args, teta, phi, seed):
+	return f"./quantum_iterations.out --start-serializing { max(0, args.n_iter - args.n_serializing + 1) } -N -T 1e-18 -n { args.n_iter } -t { teta } -p { phi } --seed { seed } " + " ".join(args.args)
 
+# print rules
 print("{")
 print("\t\"rules\" : [")
 
+n, args.n_iter = args.n_iter, 0
+
+stream = os.popen(make_cmd(args, 0, 0, 0))
+rules = json.loads(stream.read())["rules"]
+
+args.n_iter = n
+
+for i in range(len(rules)):
+	del rules[i]["teta"]
+	del rules[i]["phi"]
+
+	print_to_json(2, rules[i])
+
+print("\t],", flush=True)
+print(f"\t\"phi\" : { phis },")
+print(f"\t\"teta\" : { tetas },")
+print("\t\"results\" : [", flush=True)
+
+# iterate throught argument space
 for phi in phis:
 	for teta in tetas:
 
 		avg = None
 
 		for seed in range(args.start_seed, args.end_seed):
-			cmd = f"./quantum_iterations.out --start-serializing { args.n_iter - args.n_serializing + 1 } -N -T 1e-18 -n { args.n_iter } -t { teta } -p { phi } --seed { seed } " + " ".join(args.args)
-
-			stream = os.popen(cmd)
+			stream = os.popen(make_cmd(args, teta, phi, seed))
 			data = json.loads(stream.read())
-
-			# add rules to json
-			if rules is None:
-				rules = data["rules"]
-
-				for i in range(len(rules)):
-					del rules[i]["teta"]
-					del rules[i]["phi"]
-
-				for i, rule in enumerate(rules):
-					print("\t\t{")
-					for j, key in enumerate(rule):
-						print(f"\t\t\t\"{ key }\" : { rule[key] }{ ',' if i != len(rule) - 1 else '' }")
-					print(f"\t\t{ '},' if i != len(rules) - 1 else '}' }")
-
-				print("\t],")
-				print(f"\t\"phi\" : { phis },")
-				print(f"\t\"teta\" : { tetas },")
-				print("\t\"results\" : [")
 
 			# average over iterations
 			for it in data["iterations"]:
@@ -79,15 +96,13 @@ for phi in phis:
 			avg[key] /= n_avg
 
 		# print to json
-		print("\t\t{")
-		print(f"\t\t\t\"teta\" { teta },")
-		print(f"\t\t\t\"phi\" { phi },")
+		print("\t\t{", flush=True)
+		print(f"\t\t\t\"teta\" : { teta },")
+		print(f"\t\t\t\"phi\" : { phi },")
 		print("\t\t\t\"data\" : {")
-		for i, key in enumerate(avg):
-			print(f"\t\t\t\t\"{ key }\" : { avg[key] }{ ',' if i != len(avg) - 1 else '' }")
-		print("\t\t\t}")
-		print(f"\t\t{ '},' if teta != tetas[-1] or phi != phis[-1] else '}' }")
+		print_to_json(3, avg)
+		print(f"\t\t}}{ ',' if teta != tetas[-1] or phi != phis[-1] else '' }", flush=True)
 
-# Serializing json   
+# finish json   
 print("\t]")
 print("}")
