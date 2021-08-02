04ff0cb2463790ad04e5838f70e3ec6834b027c3
jolatechno <joseph.touzet@gmail.com>
2021-08-02 21:17:11 +0200

    changed the way time is meusured

--- v6/scalling_test/grapher.py
+++ v6/scalling_test/grapher.py
@@ -17,25 +17,22 @@ with open(filename) as f:
 n_threads = data["results"].keys()
 fig_title = data["command"]
 
-scaling = np.zeros((len(n_threads), len(data["results"]["1"]["steps"]) + 1))
-proportions = np.zeros((len(n_threads), len(data["results"]["1"]["steps"])))
+scaling = np.zeros((len(n_threads), len(data["results"]["1"]["wall"]["steps"]) + 1))
+inverse_scaling = np.zeros((len(n_threads), len(data["results"]["1"]["wall"]["steps"]) + 1))
+proportions = np.zeros((len(n_threads), len(data["results"]["1"]["wall"]["steps"])))
 
 for i, n_thread in enumerate(n_threads):
-	scaling[i, -1] = data["results"]["1"]["total"] / data["results"][n_thread]["total"]
+	scaling[i, -1] = data["results"]["1"]["wall"]["total"] / data["results"][n_thread]["wall"]["total"]
 
 	for step in range(proportions.shape[1]):
-		proportions[i, step] = data["results"][n_thread]["steps"][step] / data["results"][n_thread]["total"]
+		proportions[i, step] = data["results"][n_thread]["wall"]["steps"][step] / data["results"][n_thread]["wall"]["total"]
+		scaling[i, step] = data["results"]["1"]["wall"]["steps"][step] / data["results"][n_thread]["wall"]["steps"][step]
+		inverse_scaling[i, step] = data["results"][n_thread]["cpu"]["steps"][step] / data["results"]["1"]["cpu"]["steps"][step]
 
-		step_time = data["results"][n_thread]["steps"][step]
-		scaling[i, step] = data["results"]["1"]["steps"][step] / step_time if step_time > 0 else 1
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 13), constrained_layout=True)
 
-''' !!!TODO
-add figure saving 
-https://matplotlib.org/stable/gallery/lines_bars_and_markers/barchart.html !!! '''
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10), constrained_layout=True)
-
-# plot scalling
-ax1.set_title("scalling for each step")
+# plot wall time scalling
+ax1.set_title("execution time reduction (in wall time) for each step")
 
 bar_width = width / proportions.shape[1]
 tick_position = np.arange(len(n_threads), dtype=float)  # the label locations
@@ -51,8 +48,10 @@ bars = ax1.bar(bar_starting_position + bar_width*proportions.shape[1], scaling[:
 
 ax1.legend()
 
+
+
 # plot proportions
-ax2.set_title("proportion of the total execution time taken by each step")
+ax2.set_title("proportion of the total execution cpu time taken by each step")
 
 bar_width = width / (proportions.shape[1] - 1)
 tick_position = np.arange(len(n_threads), dtype=float)  # the label locations
@@ -64,5 +63,25 @@ ax2.set_xticklabels(n_threads)
 for step in range(proportions.shape[1]):
 	ax2.bar(bar_starting_position + bar_width*step, proportions[:, step], width=bar_width, label=f'step { step + 1 }')
 
+
+
+# plot cpu time scalling
+ax3.set_title("execution time multiplication (in cpu time) for each step")
+
+bar_width = width / proportions.shape[1]
+tick_position = np.arange(len(n_threads), dtype=float)  # the label locations
+bar_starting_position = tick_position - bar_width * proportions.shape[1] / 2
+
+ax3.set_xticks(tick_position)
+ax3.set_xticklabels(n_threads)
+
+for step in range(proportions.shape[1]):
+	ax3.bar(bar_starting_position + bar_width*step, inverse_scaling[:, step], width=bar_width, label=f'step { step + 1 }')
+
+bars = ax1.bar(bar_starting_position + bar_width*proportions.shape[1], inverse_scaling[:, -1], width=bar_width, label='total')
+
+
+
+# saving fig
 fig.suptitle(fig_title)
 fig.savefig("plots/scalling.png")
--- v6/scalling_test/state_test.cpp
+++ v6/scalling_test/state_test.cpp
@@ -1,20 +1,32 @@
-#include <chrono>
-#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
 
 /* number of step */
-#define N_STEP 9
+#define N_STEP 8
 
 /* define a custom "MID_STEP_FUNCTION(n)" that accumulate the time taken in each step */
 #define MID_STEP_FUNCTION(n) \
-	if (n > 0 && n < N_STEP) \
-		step_duration[n - 1] += std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now() - step_time[n - 1]).count() / 1e3; \
-	if (n < N_STEP - 1) \
-		step_time[n] = std::chrono::steady_clock::now();
+	if (n > 0) { \
+		step_wall_duration[n - 1] += get_wall_time(); \
+		step_cpu_duration[n - 1] += get_cpu_time(); \
+	} \
+	if (n < N_STEP) { \
+		step_wall_duration[n] -= get_wall_time(); \
+		step_cpu_duration[n] -= get_cpu_time(); \
+	}	
 
 /* global vector to accumulate */
 #define STATE_GLOBAL_HEADER \
-	std::vector<std::chrono::steady_clock::time_point> step_time(N_STEP); \
-	std::vector<double> step_duration(N_STEP, 0.0);
+	double get_wall_time(){ \
+	    struct timeval time; \
+	    gettimeofday(&time,NULL); \
+	    return (double)time.tv_sec + (double)time.tv_usec * .000001; \
+	} \
+	double get_cpu_time(){ \
+	    return (double)clock() / CLOCKS_PER_SEC; \
+	} \
+	std::vector<double> step_cpu_duration(N_STEP, 0.0); \
+	std::vector<double> step_wall_duration(N_STEP, 0.0);
 
 #include "../src/rules.hpp"
 #include "../src/utils/parser.hpp"
@@ -27,31 +39,13 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
-	std::vector<int> rand(1000000000);
-	std::iota(rand.begin(), rand.end(), 1);
-
-	int batch = rand.size() / 4;
-
-	omp_set_nested(3);
-
-	/*#pragma omp parallel for num_threads(4) schedule(static)
-	for (unsigned int i = 0; i < 4; ++i) {
-		printf("thread=%d/%d, i=%d\n", omp_get_thread_num(), omp_get_num_threads(), i);
-
-		omp_set_num_threads(2);
-
-		#pragma omp parallel
-		printf("thread=%d/%d, i=%d\n", omp_get_thread_num(), omp_get_num_threads(), i);
-
-		__gnu_parallel::sort(rand.begin() + batch*i, rand.begin() + batch*(i + 1), [](int x, int y) { return x > y; });
-	}*/
-
 	std::setvbuf(stdout, NULL, _IONBF, 0);
 
 	cxxopts::Options options("file used for multi-threading test (does not run reversed iterations");
 	auto [s, rule, _, n_iter, __, normalize, n_fast] = test_parser(options, argc, argv);
 
-	auto begin = std::chrono::steady_clock::now();
+	double total_cpu_time = -get_cpu_time();
+	double total_wall_time = -get_wall_time();
 
 	for (int i = 0; i < n_iter; ++i) {
 		if (n_fast == 0 || i % (n_fast + 1) == 0) {
@@ -62,14 +56,24 @@ int main(int argc, char* argv[]) {
 		move_all(s);
 	}
 
-	auto end = std::chrono::steady_clock::now();
+	total_cpu_time += get_cpu_time();
+	total_wall_time += get_wall_time();
 
-	printf("{ \"steps\" : [");
+	printf("{ \"wall\" : { \"steps\" : [");
 	for (unsigned int i = 0; i < N_STEP; ++i) {
-		printf("%f", step_duration[i]);
+		printf("%f", step_wall_duration[i]);
 		if (i < N_STEP - 1) {
 			printf(", ");
 		} else
-			printf("], \"total\" : %f }", std::chrono::duration<double, std::milli>(end - begin).count() / 1e3);
+			printf("], \"total\" : %f }", total_wall_time);
 	}
+	printf(", \"cpu\" : { \"steps\" : [");
+	for (unsigned int i = 0; i < N_STEP; ++i) {
+		printf("%f", step_cpu_duration[i]);
+		if (i < N_STEP - 1) {
+			printf(", ");
+		} else
+			printf("], \"total\" : %f }", total_cpu_time);
+	}
+	printf("}");
 }
