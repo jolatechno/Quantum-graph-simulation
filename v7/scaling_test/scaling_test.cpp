b41340bbcbda8fd4cb535b230b175e1f3a39ad78
jolatechno <joseph.touzet@gmail.com>
2021-10-23 14:43:29 +0200

    added a v7 made around iqs

---
+++ v7/scaling_test/scaling_test.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <chrono>
+
+#include "../IQS/src/lib/iqs.hpp"
+#include "../IQS/src/rules/qcgd.hpp"
+
+typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_point;
+
+int main(int argc, char* argv[]) {
+	iqs::it_t buffer;
+	iqs::sy_it_t sy_it;
+
+	const int num_step = 8;
+	std::vector<double> step_duration(num_step, 0.0);
+	std::vector<time_point> step_start(num_step);
+
+	auto const mid_step_function = [&](int n) {
+		std::cout << "step (" << n << ")\n";
+		if (n > 0) {
+			time_point stop = std::chrono::high_resolution_clock::now(); \
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - step_start[n - 1]); \
+			step_duration[n - 1] += duration.count() * 1e-6; \
+		}
+		if (n < num_step) {
+			step_start[n] = std::chrono::high_resolution_clock::now(); \
+		}
+	};
+
+	auto [n_iter, state, rule] = iqs::rules::qcgd::flags::parse_simulation(argv[1], iqs::utils::default_hasher, mid_step_function);
+
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < n_iter; ++i) {
+		mid_step_function(0);
+		rule(state, buffer, sy_it);
+	}
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+	/* print results as json */
+	printf("\t\"steps\" : [");
+	for (unsigned int i = 0; i < num_step; ++i) {
+		printf("%f", step_duration[i]);
+		if (i < num_step - 1) {
+			printf(", ");
+		} else
+			printf("],\n\t\"total\" : %f\n}", duration.count() * 1e-6);
+	}
+}
