7454dc905b563d5dd96b5f20106d80b65dee6c34
jolatechno <joseph.touzet@gmail.com>
2021-11-17 10:03:25 +0100

    uptated IQS and added options to compile easily on a cluster

--- get-tbb-cflags.sh
+++ get-tbb-cflags.sh
@@ -1,6 +1,24 @@
+print_usage() {
+  printf "Usage: ./get-tbb-cflags.sh ...
+	-p: something that was append by tbb-install.sh to the built path (default = \"\") 
+
+	-h: this help.
+"
+}
+
+path=""
+
+while getopts 'p:h' flag; do
+  case "$flag" in
+  	h) print_usage
+       exit 1 ;;
+	p) path="${OPTARG}" ;;
+  esac
+done
+
 CURRENT_PATH=$(pwd)
 TBB_INSTALL_DIR="${CURRENT_PATH}/oneTBB"
 TBB_INCLUDE="${TBB_INSTALL_DIR}/include"
-TBB_LIBRARY_RELEASE=${TBB_INSTALL_DIR}/build/*_release/
+TBB_LIBRARY_RELEASE=${TBB_INSTALL_DIR}/${path}build/*_release/
 
 echo -I${TBB_INCLUDE} -Wl,-rpath,$(echo ${TBB_LIBRARY_RELEASE}) -L$(echo ${TBB_LIBRARY_RELEASE})
--- tbb-install.sh
+++ tbb-install.sh
@@ -1,7 +1,48 @@
-git clone --branch tbb_2020 https://github.com/oneapi-src/oneTBB oneTBB
+#!/bin/bash
 
-echo -e "\n\ncompiling..."
-(cd oneTBB && make -j)
+print_usage() {
+  printf "Usage: ./tbb-install.sh ...
+	-c: CXXFLAGS to pass to make (default = \"\")
+	-p: something to append to the built path (default = \"\") 
 
-echo -e "\n\nDone! You can now the output of \"./get-tbb-cflags.sh\" to cflags when compiling"
+	-h: this help.
+"
+}
+
+CFLAGS=""
+path=""
+
+while getopts 'c:p:h' flag; do
+  case "$flag" in
+  	h) print_usage
+       exit 1 ;;
+    c) CFLAGS="${OPTARG}" ;;
+	p) path="${OPTARG}" ;;
+  esac
+done
+
+if [ ! -d ./oneTBB ]; then
+	git clone --branch tbb_2020 https://github.com/oneapi-src/oneTBB oneTBB
+	echo -e "\n\n"
+fi
+
+echo -e "compiling..."
+
+if [ "$CFLAGS" != "" ]; then
+	(cd oneTBB && \
+	make clean -j all CXXFLAGS=${CFLAGS})
+else
+	(cd oneTBB && \
+	make clean -j all)
+fi
+
+echo ""
+
+if [ "$path" != "" ]; then
+	echo -e "\nmoving files..."
+	rm -r ./oneTBB/${path}build
+	cp -r -f ./oneTBB/build ./oneTBB/${path}build
+fi
+
+echo -e "\nDone! You can now the output of \"./get-tbb-cflags.sh\" to cflags when compiling"
 
