f242d44274ab30926d175d30c8dd1245ca316f12
jolatechno <joseph.touzet@gmail.com>
2021-11-24 13:16:01 +0100

    added new results to mpi xcaling

--- v7/omp_mpi_scalling_test/grapher.py
+++ v7/omp_mpi_scalling_test/grapher.py
@@ -22,45 +22,58 @@ if filenames == ["--mt"]:
 	filenames.append("res.json")
 
 for filename in filenames:
+	name_extension = ""
+	if len(filename.split(",")) == 2:
+		filename, name_extension = filename.split(",")
+
 	with open(filename) as f:
 		data = json.load(f)
 
 	n_threads = list(data["results"].keys())
 	n_threads.sort(key=functools.cmp_to_key(compare))
+	num_threads = [np.product([int(x) for x in n_thread.split(',')]) for n_thread in n_threads]
+
 	rule = data["command"].split("|")[-1].replace(";", "_")
 
 	scaling = np.zeros((len(n_threads), len(data["results"][n_threads[0]]["steps"]) + 1))
 	proportions = np.zeros((len(n_threads), len(data["results"][n_threads[0]]["steps"])))
+	num_object = np.zeros(len(n_threads))
 	object_per_threads = np.zeros(len(n_threads))
 
 	for i, n_thread in enumerate(n_threads):
-		scaling[i, -1] = data["results"][n_threads[0]]["total"] / data["results"][n_thread]["total"]
+		total = data["results"][n_thread]["total"]
+
+		scaling[i, -1] = 1 if total == 0 else data["results"][n_threads[0]]["total"] / total
 		for step in range(proportions.shape[1]):
-			proportions[i, step] = data["results"][n_thread]["steps"][step] / data["results"][n_thread]["total"]
-			scaling[i, step] = data["results"][n_threads[0]]["steps"][step] / data["results"][n_thread]["steps"][step]
+			base = data["results"][n_thread]["steps"][step]
+
+			proportions[i, step] = 1 if total == 0 else data["results"][n_thread]["steps"][step] / total
+			scaling[i, step] = 1 if base == 0 else data["results"][n_threads[0]]["steps"][step] / base
 
 			total_n_thread = np.product([int(x) for x in n_thread.split(',')])
-			object_per_threads[i] = data["results"][n_thread]["num_object"] / total_n_thread
+			num_object[i] = data["results"][n_thread]["num_object"]
+			object_per_threads[i] = num_object[i] / total_n_thread
 
 
 
 
 	# limit graph values
 	x_points, y_points = [[], [], []], [[], [], []]
+	min_num_threads = num_threads[0]
 	for i in range(0, len(n_threads)):
-		n_thread, n_nodes = [int(x) for x in n_threads[i].split(',')]
-		total_n_thread = n_thread*n_nodes
+		_, n_nodes = n_threads[i].split(",")
+		max_scaling = num_threads[i]/min_num_threads
 
 		if i < len(n_threads) - 2:
-			total_n_thread_1 = np.product([int(x) for x in n_threads[i + 1].split(',')])
-			if total_n_thread_1 > total_n_thread:
+			max_scaling_1 = num_threads[i + 1]/min_num_threads
+			if max_scaling_1 > max_scaling:
 				x_points.append([i + 0.2, len(n_threads) - 1])
-				y_points.append([total_n_thread, total_n_thread])
+				y_points.append([max_scaling, max_scaling])
 
 		x_points[0].append(i - 0.2)
-		y_points[0].append(total_n_thread)
+		y_points[0].append(max_scaling)
 		x_points[0].append(i + 0.2)
-		y_points[0].append(total_n_thread)
+		y_points[0].append(max_scaling)
 
 		x_points[1].append(i - 0.2)
 		y_points[1].append(n_nodes)
@@ -75,7 +88,7 @@ for filename in filenames:
 	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
 	ax.set_title("execution time scaling for each step")
 	ax.set_xticks(np.arange(0, len(n_threads)))
-	ax.set_xticklabels([np.product([int(x) for x in n_thread.split(',')]) for n_thread in n_threads])
+	ax.set_xticklabels(num_threads)
 
 	for step in range(proportions.shape[1]):
 		ax.plot(scaling[:, step], label=f'step { step + 1 }')
@@ -83,7 +96,7 @@ for filename in filenames:
 	ax.plot(scaling[:, -1], linewidth=4, label='total')
 
 	max_scalling = np.amax(scaling)
-	if max_scalling > y_points[0][-1] / 2:
+	if max_scalling > np.sqrt(y_points[0][-1]):
 		ax.plot(x_points[0], y_points[0], "k--", label='total number of thread')
 		ax.plot(x_points[1], y_points[1], "r--", label='number of mpi ranks')
 		for i in range(3, len(x_points)):
@@ -96,7 +109,7 @@ for filename in filenames:
 	# saving fig
 	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 	ax.set_xlim(0, len(n_threads) - 1)
-	fig.savefig("plots/scaling/scaling_" + rule + ".png")
+	fig.savefig("plots/scaling/scaling_" + rule + name_extension + ".png")
 
 
 
@@ -106,7 +119,7 @@ for filename in filenames:
 	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
 	ax.set_title("proportion of the total execution time taken by each step")
 	ax.set_xticks(np.arange(0, len(n_threads)))
-	ax.set_xticklabels(n_threads)
+	ax.set_xticklabels([np.product([int(x) for x in n_thread.split(',')]) for n_thread in n_threads])
 
 	for step in range(proportions.shape[1]):
 		ax.plot(proportions[:, step], label=f'step { step + 1 }')
@@ -114,7 +127,7 @@ for filename in filenames:
 	# saving fig
 	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 	ax.set_xlim(0, len(n_threads) - 1)
-	fig.savefig("plots/proportions/proportions_" + rule + ".png")
+	fig.savefig("plots/proportions/proportions_" + rule + name_extension + ".png")
 
 
 
@@ -122,15 +135,17 @@ for filename in filenames:
 
 	# plot properties
 	fig, ax = plt.subplots(1, 1, figsize=(10, 5), constrained_layout=True)
+	ax2 = ax.twinx()
 	ax.set_title("number of object per thread")
 	ax.set_xticks(np.arange(0, len(n_threads)))
-	ax.set_xticklabels(n_threads)
+	ax.set_xticklabels([np.product([int(x) for x in n_thread.split(',')]) for n_thread in n_threads])
 
-	ax.plot(object_per_threads, label=f'number of object per thread')
+	ax.plot(object_per_threads, "b-", label='number of object per thread')
+	ax2.plot(num_object, "r-", label='total number of object')
 
 	# saving fig
 	ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 	ax.set_xlim(0, len(n_threads) - 1)
 	ymin, ymax = ax.get_ylim()
 	ax.set_ylim(0, ymax)
-	fig.savefig("plots/properties/properties_" + rule + ".png")
+	fig.savefig("plots/properties/properties_" + rule + name_extension + ".png")
