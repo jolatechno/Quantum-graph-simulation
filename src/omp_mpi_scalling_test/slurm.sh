e9c38240dd3b45d61367b1b71c6beaca5697150d
jolatechno <joseph.touzet@gmail.com>
2022-03-25 17:06:06 +0100

    trying to modifie slurm script

--- src/omp_mpi_scalling_test/slurm.sh
+++ src/omp_mpi_scalling_test/slurm.sh
@@ -26,7 +26,58 @@ echo “As the user: $USER”
 
 echo -e "\n===== job results ====\n"
 
-./scaling_test.sh -m "${MPI_ARGS}" -N ${SLURM_JOB_NUM_NODES} -n ${n_per_node} -t ${n_threads} -a ${rule} -f ${NAME}
+temp_file=$(mktemp)
+
+command="${file} ${args}"
+echo "{"
+echo "  \"mpi_command\" : \"srun --cpus-per-task=${n_thread} --task=${n_node} ${mpirun_args}\","
+echo "	\"command\" : \"${command}\","
+echo "	\"results\" : {"
+
+last_element=$((${#n_threads[@]} - 1))
+for i in "${!n_threads[@]}"; do
+	n_thread=${n_threads[i]}
+	n_node=${n_nodes[i]}
+	total_n_node=$(($n_node * $total_n_nodes))
+
+	separator=""
+	if (( $i < $last_element )); then
+		separator=","
+	fi
+
+	echo "		\"${n_thread},${n_node}\" : {"
+	
+	for map_by in ldoms sockets boards; do #hwthread core L3cache
+		>&2 echo -e "\n\n\n${n_thread},${n_node} (${map_by}):"
+
+		start=`date +%s.%N`
+		srun --cpu-bin=${map_by} --cpus-per-task=${n_thread} --task=${n_node} ${mpirun_args} ${command} > ${temp_file}
+
+		# delete core-dump file to free-up memory
+		rm -f core.*
+
+		exit_code=$?
+		runtime=$( echo "`date +%s.%N` - $start" | bc -l )
+
+		if [ "$exit_code" -eq 0 ]; then
+			echo "$(cat ${temp_file})${separator}" | indent | indent 
+			break
+		fi
+
+		if (( $(echo "$runtime > $timeLimit" |bc -l) )); then
+			echo -e "\t\"total\" : $runtime\n}" | indent | indent 
+			break
+		fi
+	done
+
+	>&2 echo -e "\n\n\n"
+done
+
+echo "	}"
+echo "}"
+
+
+#./scaling_test.sh -m "${MPI_ARGS}" -N ${SLURM_JOB_NUM_NODES} -n ${n_per_node} -t ${n_threads} -a ${rule} -f ${NAME}
 
 #end job
 exit 0
